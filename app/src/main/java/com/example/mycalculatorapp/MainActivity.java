136257602f20038caf0738320b5b2e905ca083d6
sawan18 <sawan.pandita18@gmail.com>
2022-08-24 18:46:13 -0500

    Added the percentage operation sucessfully. For the square root it need a tweak in the rounding. Get the second page to work. Figure out the 3 extra enhancements.

--- app/src/main/java/com/example/mycalculatorapp/MainActivity.java
+++ app/src/main/java/com/example/mycalculatorapp/MainActivity.java
@@ -72,4 +72,31 @@ public class MainActivity extends AppCompatActivity {
         numberProductTV.setText("" + quotient);
     }
 
+    public void findSecRoot(View view) {
+        EditText number1ET = findViewById(R.id.enterNumberOne);
+        EditText number2ET = findViewById(R.id.enterNumberTwo);
+        TextView numberSecondRootTV = findViewById(R.id.numAnswerDisplay);
+
+        double num1 = Double.parseDouble(number1ET.getText().toString());
+        double num2 = Double.parseDouble(number2ET.getText().toString());
+        //double secondRoot = num1 / num2;
+        double a = Math.pow(num2, 1.0 / num1);
+        double b = Math.round(a);
+
+        numberSecondRootTV.setText("" + b);
+    }
+
+    // Percentage = (Obtained score x 100) / Total Score
+    public void findPercent(View view) {
+        EditText number1ET = findViewById(R.id.enterNumberOne);
+        EditText number2ET = findViewById(R.id.enterNumberTwo);
+        TextView numberSecondRootTV = findViewById(R.id.numAnswerDisplay);
+
+        double num1 = Double.parseDouble(number1ET.getText().toString());
+        double num2 = Double.parseDouble(number2ET.getText().toString());
+        double secondRoot = (num1/num2)*100;
+
+        numberSecondRootTV.setText("" + secondRoot);
+    }
+
 }
